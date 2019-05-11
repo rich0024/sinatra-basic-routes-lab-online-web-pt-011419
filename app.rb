cc1e36634ff0062b3848b6f1e47e8c18f01e01f8
Richard Yance <yance0022@gmail.com>
2019-05-11 20:16:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Rich"
+  end
+
+  get '/hometown' do
+    "My hometown is Montclair, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Time Will Tell"
+  end
+
 end
